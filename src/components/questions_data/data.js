8e7736cfdfbe2074bac80f8c3a5a1aeda0661cf5
Swetha Kammari <swethakammari7@gmail.com>
2023-03-27 13:35:22 +0530

    first commit

---
+++ src/components/questions_data/data.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        question: 'PREDICT THE TOP LOSER (for tomorrow) across these indices',
+        answers: [
+            'NIFTY',
+            'NIFTYNEXT50',
+            'NIFTY BANK',
+        ],
+        correct: 1
+    },
+
+    {
+        question: 'The Oldest Stock Market in India is:',
+        answers: [
+            'DSE',
+            'BSE',
+            'NSE',
+            
+        ],
+        correct: 1
+    },    
+    {
+        question: 'The Indian Stock Market is regulated by:',
+        answers: [
+            'Reserve Bank of India (RBI)',
+            'Government of India (GOI)', 
+            'Securities and Exchange Board of India (SEBI)', 
+            
+        ],    
+        correct: 1
+    },    
+    {
+        question: 'The Largest Stock Market in India is:',
+        answers: [
+            'Bombay Stock Exchange (BSE)',
+            'National Stock Exchange (NSE)', 
+            'Delhi Stock Exchange (DSE)', 
+            
+        ],    
+        correct: 2
+    },
+    {
+        question: 'NIFTY carries',
+        answers: [
+            'Top 50 Companies from India',
+            'Top 50 Companies Listed on NSE', 
+            'Top 30 Companies form BSE', 
+        ],    
+        correct: 1
+    },
+    
+]
